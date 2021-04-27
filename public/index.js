027eedc14f3667655517b51d74eaa2b719e602c8
xczq <290353142@qq.com>
2021-04-27 18:01:10 +0800

    db

---
+++ public/index.js
@@ -0,0 +1,22 @@
+System.config({
+    packages:{
+        "vue":111
+    }
+})
+
+const App = {
+    data() {
+        return {
+            message: "HelloWorld"
+        }
+    }
+}
+
+/**
+ * 创建加载动画
+ * 
+ * 
+ */
+function aut(){}
+
+Vue.createApp(App).mount('#app')
